d4042731d9c6b8bf4fc83c8401f9bb9b310e400c
Milian20 <boehm.milian@gmail.com>
2024-10-29 16:20:42 +0100

    Add wake up timer and ad Red display for  low water percent

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,14 @@
 
 
 TFT_eSPI tft = TFT_eSPI();
+TFT_eSprite BG = TFT_eSprite(&tft);
 TFT_eSprite Waterpercenticon = TFT_eSprite(&tft);
 TFT_eSprite WaterpercentNummber = TFT_eSprite(&tft);
 TFT_eSprite Blume = TFT_eSprite(&tft);
 
 #define TFTDISPLAYPOWER 32 // PIN from ESP
 #define BUTTON_PIN 39   // PIN from ESP for deep slep and wacke up
-#define timer_deep_sleep_in_hours 4*3600 // hour * 3600 seconds for 1 hour
+#define timer_deep_sleep_in_hours 10//*3600 // hour * 3600 seconds for 1 hour
 
 void setup() {
  Serial.begin(115200);
@@ -44,6 +45,11 @@ delay(2000);
   tft.setSwapBytes(true);
   tft.fillScreen(TFT_DARKGREY);
 
+  BG.createSprite(113,240);
+  BG.setSwapBytes(true);
+  BG.fillSprite(TFT_TRANSPARENT);
+  BG.pushSprite(127, 0);
+
   // draw water icon on tft
   Waterpercenticon.createSprite(96, 96);
   Waterpercenticon.setSwapBytes(true);
@@ -69,7 +75,7 @@ long previusblumenanzeigenummer = -1;
 void loop() {
   digitalWrite(TFTDISPLAYPOWER, HIGH);
   Serial.println();
-  
+
     // deepsleep check
   if (digitalRead(BUTTON_PIN) == LOW && blumenanzeigenummer != 1) {
     Serial.println("Knopf gedrückt, gehe in den Deep-Sleep");
@@ -88,6 +94,8 @@ void loop() {
     esp_deep_sleep_start();
   }
 
+
+
 // water Sennsor input
   long analogwert= analogRead(A0);
   Serial.print("Analogwert: ");
@@ -95,11 +103,31 @@ void loop() {
   Serial.println();
   prozentwert = map(analogwert, 900, 2500, 100, 0);
 
+  // change watter icon if needed
+  blumenanzeigenummer = map(prozentwert,100, 0, 5, 0);
 
   // change watterpercent number if nedet
   if (prozentwert != previuswaterpercentwert) {
-    WaterpercentNummber.fillSprite(TFT_DARKGREY); 
-    WaterpercentNummber.setTextColor(TFT_BLACK, TFT_DARKGREY);
+    
+    if (blumenanzeigenummer ==1 ){
+      WaterpercentNummber.fillSprite(TFT_RED); 
+      WaterpercentNummber.setTextColor(TFT_BLACK, TFT_RED);
+
+      BG.setSwapBytes(true);
+      BG.fillSprite(TFT_RED);
+      BG.pushSprite(127, 0);
+      Waterpercenticon.pushSprite(140, 110, TFT_BLACK);
+    } 
+    else{
+      WaterpercentNummber.fillSprite(TFT_DARKGREY); 
+      WaterpercentNummber.setTextColor(TFT_BLACK, TFT_DARKGREY);
+
+      BG.setSwapBytes(true);
+      BG.fillSprite(TFT_DARKGREY);
+      BG.pushSprite(127, 0);
+      Waterpercenticon.pushSprite(140, 110, TFT_BLACK);
+    }
+    
     
     // draw new number
     WaterpercentNummber.drawNumber(prozentwert, 0,0);
@@ -111,29 +139,32 @@ void loop() {
     previuswaterpercentwert = prozentwert;
   }
 
-  // change watter icon if needed
-  blumenanzeigenummer = map(prozentwert,100, 0, 5, 0);
+
 
   if (blumenanzeigenummer != previusblumenanzeigenummer) {
-    Blume.fillSprite(TFT_DARKGREY); 
+    if (blumenanzeigenummer == 1){
+      Blume.fillSprite(TFT_RED); 
+    }
+    else
+      Blume.fillSprite(TFT_DARKGREY); 
     Blume.pushSprite(0, 0, TFT_WHITE);
     
     switch(blumenanzeigenummer){
       //0-20%
       case(1):
         Blume.setSwapBytes(true);
+        Blume.fillSprite(TFT_RED);
         Blume.pushImage(0, 0, 127, 240, Blume_Gemalt_0_20_127_240);
         Blume.pushSprite(0, 0, TFT_WHITE);
-        tft.fillScreen(TFT_RED);
         break;
     }
     //20-40%
     switch(blumenanzeigenummer){
       case(2):
         Blume.setSwapBytes(true);
+        Blume.fillSprite(TFT_DARKGREY);
         Blume.pushImage(0, 0, 127, 240, Blume_Gemalt_20_40_127_240);
         Blume.pushSprite(0, 0, TFT_WHITE);
-        tft.fillScreen(TFT_DARKGREY);
         break;
     }
     //40-60%
@@ -142,7 +173,6 @@ void loop() {
         Blume.setSwapBytes(true);
         Blume.pushImage(0, 0, 127, 240, Blume_Gemalt_40_60_127_240);
         Blume.pushSprite(0, 0, TFT_WHITE);
-        tft.fillScreen(TFT_DARKGREY);
         break;
     }
     //60-80%
@@ -151,7 +181,6 @@ void loop() {
         Blume.setSwapBytes(true);
         Blume.pushImage(0, 0, 127, 240, Blume_Gemalt_60_80_127_240);
         Blume.pushSprite(0, 0, TFT_WHITE);
-        tft.fillScreen(TFT_DARKGREY);
         break;
     }
     //80-100%
@@ -160,7 +189,6 @@ void loop() {
         Blume.setSwapBytes(true);
         Blume.pushImage(0, 0, 127, 240, Blume_Gemalt_80_100_127_240);
         Blume.pushSprite(0, 0, TFT_WHITE);
-        tft.fillScreen(TFT_DARKGREY);
         break;
     }
 
@@ -169,15 +197,7 @@ void loop() {
   }
 
 
-
-
-
-  delay(500); 
-  analogwert--;
-  if (prozentwert >99)
-  {
-    analogwert = 615;
-  }
+delay(200);
   Serial.print("prozentwert: ");
   Serial.print(prozentwert);
   Serial.println();
